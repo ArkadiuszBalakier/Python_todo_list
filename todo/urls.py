35f8c8aaaf82f619df5024263a74b54abdbcb52d
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2026-01-30 12:10:11 +0100

    feat: add urls for task delete and update

--- todo/urls.py
+++ todo/urls.py
@@ -2,13 +2,15 @@ from django import views
 from django.urls import path
 
 from todo.views import IndexView, TagsView, TagDeleteView, TagUpdateView, TagCreateView, TaskCreateView, \
-    ToggleTaskCompleteView
+    ToggleTaskCompleteView, TaskUpdateView, TaskDeleteView
 
 app_name = "todo"
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path("tags/", TagsView.as_view(), name='tags'),
     path("task/create/", TaskCreateView.as_view(), name='task-create'),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name='task-update'),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name='task-delete'),
     path("task/<int:pk>/toggle", ToggleTaskCompleteView.as_view(), name='task-toggle'),
     path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name='tags-delete'),
     path("tag/<int:pk>/update/", TagUpdateView.as_view(), name='tags-update'),
