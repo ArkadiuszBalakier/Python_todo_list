49a668771ec297b6f897d83b0219be8646018daf
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2026-01-30 10:44:34 +0100

    feat: add delete tag button and functionality

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django import views
 from django.urls import path
 
-from todo.views import IndexView, TagsView
+from todo.views import IndexView, TagsView, TagDeleteView
 
 app_name = "todo"
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path("tags/", TagsView.as_view(), name='tags'),
+    path("tag/<int:pk>/", TagDeleteView.as_view(), name='tags-delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, DeleteView
 
 from todo.models import Task, Tag
 
@@ -15,3 +16,8 @@ class IndexView(ListView):
 
 class TagsView(ListView):
     model = Tag
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    success_url = reverse_lazy('todo:tags')
