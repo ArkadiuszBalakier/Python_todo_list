5ce6d39d62e56804a9849e53b843730e13e265f6
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2026-01-30 10:22:07 +0100

    feat: add full data vie for tasks

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from todo.models import Task
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    model = Task
     template_name = 'todo/index.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        return Task.objects.prefetch_related('tags').all()
